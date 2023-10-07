f486c814d2b34cf8dcc4d25966db807b8eca9a62
Комлев Данила <komlevdanila742@gmail.com>
2023-10-07 19:33:45 +0300

    practice 1

--- practice_1/task_3.py
+++ practice_1/task_3.py
@@ -8,6 +8,30 @@ BMI = вес/рост**2
 weight = input()
 height = input()
 
+# Ваш кол
 
-#Ваш кол
+error = ""
 
+if not weight.isnumeric():
+    error = "Вес должен быть числом"
+
+if not height.isnumeric():
+    error = "Рост должен быть числом"
+
+if error != "":
+    print(error)
+
+else:
+    weight = int(weight)
+    height = int(height)
+
+    if weight <= 0:
+        error = "вес должен быть больше нуля"
+
+    if height <= 0:
+        error = "рост должен быть больше нуля"
+
+    if error == "":
+        print(f"BMI: {weight/height**2}")
+    else:
+        print(error)
